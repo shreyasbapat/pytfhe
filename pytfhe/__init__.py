061f33734ab57f76369a8c2b6e0b2fb23b4fa487
Shreyas Bapat <bapat.shreyas@gmail.com>
2020-09-26 04:52:50 +0530

    Initial Setup

---
+++ pytfhe/__init__.py
@@ -0,0 +1,6 @@
+"""
+pytfhe
+~~~~~~
+The pytfhe package - a Python Wrapper around TFHE.
+Fastest Full Homomorphic Encryption in Python.
+"""
---
+++ pytfhe/__version__.py
@@ -0,0 +1,13 @@
+"""
+__version__.py
+~~~~~~~~~~~~~~
+Information about the current version of the pytfhe package.
+"""
+
+__title__ = 'pytfhe'
+__description__ = 'Python Wrapper for TFHE'
+__version__ = '0.1.0'
+__author__ = 'Shreyas Bapat'
+__author_email__ = 'bapat.shreyas@gmail.com'
+__license__ = 'BSD 3-Clause'
+__url__ = 'https://github.com/shreyasbapat/pytfhe'
